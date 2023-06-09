7ddcec9ed62514f6af5d321ab68e81c0c1f7ff67
Abhi97shek <abhi.kumar736@gmail.com>
2023-06-10 02:16:15 +0530

    Working on Search Component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,7 +9,7 @@ function About(){
                 No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.
             </p>
             <p>
-            Another writing challenge can be to take the individual sentences in the random paragraph and incorporate a single sentence from that into a new paragraph to create a short story. Unlike the random sentence generator, the sentences from the random paragraph will have some connection to one another so it will be a bit different. You also won't know exactly how many sentences will appear in the random paragraph.
+            Another writing challenge can be to take the individual sentences in the random paragraph and incorporate a single sentence from that into a new paragraph to create a short story. Unlike the random sentence generator, the sentences from the random paragraph will have some connection to one another so it will be a bit different. You also wont know exactly how many sentences will appear in the random paragraph.
             </p>
         </h1>
     </div>
